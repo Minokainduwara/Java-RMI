7183abcd66b9e5c8968b72c3419a744614e4aec0
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-06 01:08:57 +0530

    Feat: Product Implementation

--- src/server/ProductImpl.java
+++ src/server/ProductImpl.java
@@ -1,4 +1,29 @@
 package server;
 
-public class ProductImpl {
+import java.rmi.RemoteException;
+
+public class ProductImpl implements Product {
+    private String name;
+    private String description;
+    private double price;
+
+    public ProductImpl(String name, String description, double price) throws RemoteException {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() throws RemoteException {
+        return this.name;
+    }
+
+    public String getDescription() throws RemoteException {
+        return this.description;
+    }
+
+    public double getPrice() throws RemoteException {
+        return this.price;
+    }
+
+
 }
