f38d767946fdcffbc35251b64eaac0d7f1a3ed43
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-07 01:22:04 +0530

    Feat : Remote interface for product operations

--- src/client/Product.java
+++ src/client/Product.java
@@ -1,4 +1,12 @@
 package client;
 
-public class Product {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Product extends Remote {
+    // Define API
+
+    String getName() throws RemoteException;
+    String getDescription() throws RemoteException;
+    double getPrice() throws RemoteException;
 }
