ba29cacac2148050980b0462a08a5320b6989bb7
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-06 05:46:09 +0530

    Feat: Export Product Objects

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
     public static void main(String[] args) {
@@ -11,6 +12,12 @@ public class Server {
             ProductImpl p2 = new ProductImpl("Product 2", "Description of Product 2", 10.99);
             ProductImpl p3 = new ProductImpl("Product 3", "Description of Product 3", 10.99);
             ProductImpl p4 = new ProductImpl("Product 4", "Description of Product 4", 10.99);
+
+            //Export p1,p2,p3 and p4 objects using UnicastRemoteObject class
+            Product stub1 = (Product) UnicastRemoteObject.exportObject(p1, 0);
+            Product stub2 = (Product) UnicastRemoteObject.exportObject(p2, 0);
+            Product stub3 = (Product) UnicastRemoteObject.exportObject(p3, 0);
+            Product stub4 = (Product) UnicastRemoteObject.exportObject(p4, 0);
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
