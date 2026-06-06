d245f869b89f6d5ad31743f78d29f35dd6c4a0e4
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-06-06 06:05:26 +0530

    Feat: Get the reference of exported object from RMI Registry

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,18 @@
 package client;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            // Get the reference of exported object from RMI Registry
+            // Locate the registry
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1",1099);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
